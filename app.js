4283d1db87dd21d67ec588b764054a94fab35b57
김형태 <rlarnsaas123@naver.com>
2023-03-04 22:51:48 +0900

    경로 설정 확인

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('AWS exercise의 A project입니다.');
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(3000, () => {
